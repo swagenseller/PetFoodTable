c93477af60a6d9f4629580ad0a2aeac797e6f11c
Jeff <jwagense@ucsc.edu>
2020-05-05 13:48:24 -0700

    Updates

---
+++ precache-manifest.a18d32d74ce9ff3e5523a941f7f57144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac885d0c826c12293ffbb7f94eb841a",
+    "url": "/PetFoodTable/index.html"
+  },
+  {
+    "revision": "ee3f46dae09423d1d50b",
+    "url": "/PetFoodTable/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8c60d07ee0171db808e4",
+    "url": "/PetFoodTable/static/css/main.36d257fb.chunk.css"
+  },
+  {
+    "revision": "ee3f46dae09423d1d50b",
+    "url": "/PetFoodTable/static/js/2.b639ebdf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PetFoodTable/static/js/2.b639ebdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c60d07ee0171db808e4",
+    "url": "/PetFoodTable/static/js/main.43dbfd15.chunk.js"
+  },
+  {
+    "revision": "43fe5e4269a5bf62089b",
+    "url": "/PetFoodTable/static/js/runtime-main.4182dcb7.js"
+  }
+]);
